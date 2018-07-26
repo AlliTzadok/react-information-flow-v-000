8c931e1cfbef1d0449ac645d5aa3fada4ba0c37d
Alli Tzadok <allisontzadok@gmail.com>
2018-07-26 13:13:36 -0500

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -34,7 +34,7 @@ export default class Tier1 extends Component {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={this.updateColors} className="tier1" style={backgroundColor: this.state.color, color: this.state.color}>
+        <div onClick={this.updateColors} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
       <Tier2 color={this.state.childColor} handleChildClick={this.updateChildColors}/>
       <Tier2 color={this.state.childColor} handleChildClick={this.updateChildColors}/>
       </div>
